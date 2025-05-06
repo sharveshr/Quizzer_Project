1362da1f01c7d90eaa49db836979e14e98453fbb
Sharvesh Ramesh <100819033+sharveshr@users.noreply.github.com>
2025-05-06 17:52:44 +0530

    third commit

---
+++ quizzer/src/table.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Quizproject;
+USE QuizProject;
+CREATE TABLE Quizzer(
+qno INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+questions VARCHAR(100),
+optiona VARCHAR(100),
+optionb VARCHAR(100),
+optionc VARCHAR(100),
+optiond VARCHAR(100),
+answer VARCHAR(100));
